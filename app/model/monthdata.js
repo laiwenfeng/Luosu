0a2c3b991f8bf4410d158b8703604182bc1a2496
laiwenfeng <762390861@qq.com>
2021-05-14 13:43:16 +0800

    注释MongoDB

--- app/model/monthdata.js
+++ app/model/monthdata.js
@@ -1,20 +1,21 @@
 'use strict';
 
 module.exports = app => {
-    const mongoose = app.mongoose
-    const Schema = mongoose.Schema
-    const Monthdata = new Schema({
-        name: { type: String },                    // 姓名
-        dept: { type: String },                    // 部门
-        month: { type: String },                   // 月份
-        rank: { type: Number, default: 0 },        // 排名
-        deptrank: { type: Number, default: 0 },    // 部门排名
-        hstime: { type: Number, default: 0 },      // 核算工时
-        bztime: { type: Number, default: 0 },      // 标准工时
-        cehstime: { type: Number, default: 0 },    // 超额工时
-        cekzltime: { type: String, },              // 超额控制率
-        teamtime: { type: Number, default: 0 }     // 团队工时
-    })
+    // const mongoose = app.mongoose
+    // const Schema = mongoose.Schema
+    // const Monthdata = new Schema({
+    //     name: { type: String },                    // 姓名
+    //     dept: { type: String },                    // 部门
+    //     month: { type: String },                   // 月份
+    //     rank: { type: Number, default: 0 },        // 排名
+    //     deptrank: { type: Number, default: 0 },    // 部门排名
+    //     hstime: { type: Number, default: 0 },      // 核算工时
+    //     bztime: { type: Number, default: 0 },      // 标准工时
+    //     cehstime: { type: Number, default: 0 },    // 超额工时
+    //     cekzltime: { type: String, },              // 超额控制率
+    //     teamtime: { type: Number, default: 0 }     // 团队工时
+    // })
 
-    return mongoose.model('Monthdata', Monthdata, 'monthdata')
+    // return mongoose.model('Monthdata', Monthdata, 'monthdata')
+    return null
 }
--- app/model/worktime.js
+++ app/model/worktime.js
@@ -1,13 +1,14 @@
 'use strict';
 
 module.exports = app => {
-    const mongoose = app.mongoose
-    const Schema = mongoose.Schema
-    const Worktime = new Schema({
-        name: { type: String },                    // 姓名
-        workday: { type: String },                 // 工作日
-        worktime: { type: Number, default: 0  },       // 考勤时长
-    })
+    // const mongoose = app.mongoose
+    // const Schema = mongoose.Schema
+    // const Worktime = new Schema({
+    //     name: { type: String },                    // 姓名
+    //     workday: { type: String },                 // 工作日
+    //     worktime: { type: Number, default: 0  },       // 考勤时长
+    // })
 
-    return mongoose.model('Worktime', Worktime, 'worktime')
+    // return mongoose.model('Worktime', Worktime, 'worktime')
+    return null
 }
